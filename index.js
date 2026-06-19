2b6f0682f66459709c289335aa2b808346967f57
Saharier Omi <gsaharier761@gmail.com>
2026-06-19 22:16:31 +0600

    feat: integrate MongoDB client with connection handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -7,6 +8,36 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("✅ Pinged your deployment. You successfully connected to MongoDB!");
+
+    
+
+
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error);
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
   res.send("🚀 LegalEase Server is running smoothly!");
